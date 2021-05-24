c9ba3e796c2f31a62a7e1cfb8862b635fbb4b493
Atef <58571715+IMagwaI@users.noreply.github.com>
2021-05-24 22:49:37 +0100

    edit environment file of production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBXL5sXZxHXXIZq7iQG-deA4QjeS1xV0Jw",
+    authDomain: "angular-app-244c7.firebaseapp.com",
+    projectId: "angular-app-244c7",
+    storageBucket: "angular-app-244c7.appspot.com",
+    messagingSenderId: "898486227410",
+    appId: "1:898486227410:web:07fe708e83066af232780b"
+  }
 };
